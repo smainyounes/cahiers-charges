61b68194eb7a4f56a35088c1964ae1a63e2fcb72
smainyounes <younes.abd.smain@gmail.com>
2026-04-17 00:46:09 +0200

    adding frontend prototype

---
+++ car-parts/prop/js/main.js
@@ -0,0 +1,6 @@
+// js/main.js
+
+document.addEventListener('DOMContentLoaded', () => {
+    // سيتم إضافة التفاعلات الديناميكية هنا في المراحل القادمة
+    console.log('تم تحميل النظام بنجاح - Motors Parts DZ');
+});
